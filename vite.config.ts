52f37e2b9201aff2962453dfb00d483945d09c97
Sudharma <sudharmasuvarna@gmail.com>
2025-09-13 16:28:27 +0000

    IC

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/MyPortfolio',
   server: {
     host: "::",
     port: 8080,
-    base: '/MyPortfolio/',
   },
   plugins: [
     react(),
